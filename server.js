20dc06f5c9ee416fa75e8b879be0d011c93ba5db
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-04-29 19:51:28 +0800

    Created basic server, created package.json

--- server.js
+++ server.js
@@ -10,27 +10,3 @@ app.use(express.json());
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-
-  let reservations = [
-    {
-
-    }
-  ]
-
-  let waitlist = [
-    {
-
-    }
-  ]
-
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
-
-app.get("/reservation", function(req, res) {
-  res.sendFile(path.join(__dirname, "make.html"));
-});
-
-app.get("/view", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
-});

e112f13c0b17e71ec303ace289c030b57f19bef2
Max Simpson <maxsimpson95@gmail.com>
2020-04-29 19:57:25 +0800

    added reservation and reservationRepo class

---
+++ reservation.js
@@ -0,0 +1,10 @@
+class Reservation {
+    constructor(id,time){
+        this.id = id
+        this.time = time
+        this.email = email
+        this.phoneNumber = phoneNumber
+    }
+}
+
+module.exports = Reservation;
